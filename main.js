22a351ce48889812396fd8fefbbc568f7908f75d
JoshuaLyon <joshua.t.lyon@gmail.com>
2022-11-12 11:12:57 +0000

    rewrite to use sequential rainbow rather than rand

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-let RandomInt = (min, max) => {
-    let range = max-min;
-    return Math.random() * range + min;
-}
+// let RainbowTablesRandomInt = (min, max) => {
+//     let range = max-min;
+//     return Math.random() * range + min;
+// }
 
-let RandomColor = () => `hsl(${RandomInt(0,360)},100%,50%)`;
+// let RainbowTablesRandomColor = () => `hsl(${RainbowTablesRandomInt(0,360)},100%,72%)`;
 
 let RainbowTable = () => {
     let rows = document.getElementsByTagName('tr');
+    let colorHue = 0;
     for (let row of rows){
-        row.style.background = RandomColor();
+        let color = `hsl(${colorHue},100%,72%)`;
+        row.style.background = color;
+        let children = row.children;
+        for (let child of children){
+            child.style.background = color;
+        }
+        colorHue = colorHue + 30 % 330
     }
 }
 
